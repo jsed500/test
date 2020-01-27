9e478e350df7a1878d9bf05b1f8671a101907c9e
ashdyson8 <59566987+ashdyson8@users.noreply.github.com>
2020-01-27 23:05:49 +0000

    Add files via upload

---
+++ MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+          
+            return View();
+        }
+            
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortby={1}", pageIndex, sortby));
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+       
+    }
+}
